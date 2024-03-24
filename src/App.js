5df3042b5e6abd416bd94c62a47cab35ddba02a9
haitianlan <31479870+v2vv@users.noreply.github.com>
2024-03-24 07:14:52 +0000

    导入高德jsAPI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import InputDialog from "./Compment/InputDialog.js";
+import MapContainer from "./Compment/MapContainer.js";
 
 function App() {
   return (
     <div className="App">
+      <MapContainer></MapContainer>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <InputDialog></InputDialog>
+        </div>
+
         <a
           className="App-link"
           href="https://reactjs.org"
